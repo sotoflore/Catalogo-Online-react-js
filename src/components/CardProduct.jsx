3131586fbb4fa46c134a8ccdc41f95aaa8fea7c3
Francisco Soto <sotofloresfrancisco@gmail.com>
2024-01-28 10:38:22 -0500

    cambiando numero telefonico del whatsapp

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -11,7 +11,7 @@ const CardProduct = ({ product, onShowDetails }) => {
         <Card.Title className='fs-6 fw-bold'>{product.name}</Card.Title>
         <p className='badge text-bg-primary'><small>{product.category}</small></p>
         <p className='text-black fw-bold text-small'>Precio: <span className='badge text-bg-danger p-2'>S/{product.price}.00</span></p>
-        <Button href={`https://wa.me/3127393740?text=Hola,%20Deseo%20más%20información%20de%20este%20producto:%20*${product.name}*%20-%20${product.description}%20-%20Precio:%20*S/${product.price}.00*`} className='w-100 mt-3 bg-success border-0 text-white d-flex align-items-center justify-content-center text-small' target="_blank" rel="noopener noreferrer"><IconWhatsapp/><span className='ps-1'>Comprar</span></Button>
+        <Button href={`https://wa.me/+51902295945?text=Hola,%20Deseo%20más%20información%20de%20este%20producto:%20*${product.name}*%20-%20Precio:%20*S/${product.price}.00*`} className='w-100 mt-3 bg-success border-0 text-white d-flex align-items-center justify-content-center text-small' target="_blank" rel="noopener noreferrer"><IconWhatsapp/><span className='ps-1'>Comprar</span></Button>
         <Button onClick={() => onShowDetails(product)} className='w-100 mt-2 bg-black border-0 text-small text-white' >Ver Detalles</Button>
       </Card.Body>
     </Card>
