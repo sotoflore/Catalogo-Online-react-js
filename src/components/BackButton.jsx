8d10eb33b1e7c4d9cae38b4fac010159a4cde2fd
Francisco Soto <sotofloresfrancisco@gmail.com>
2024-02-13 17:44:47 -0500

    modificando el boton de regresar

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
+import IconArrow from "./icons/IconArrow";
 
 
 const BackButton = () => {
   return (
     <div className="container p-0">
-        <NavLink to="/" className="bg-header border-0 text-white px-3 py-2 rounded-3 text-decoration-none d-block d-lg-inline-block mt-4 mb-3 mb-lg-0">🡠 Volver</NavLink>
+        <NavLink to="/" className="bg-header border-0 text-white px-3 py-2 rounded-3 text-decoration-none  d-inline-flex align-items-center mt-4 mb-3 mb-lg-0 fw-bold"><IconArrow/><span className="ps-1">Volver</span></NavLink>
     </div>
   )
 }
